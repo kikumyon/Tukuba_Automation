455667f2eb9d142f0e075262c920cef21395fee6
momeemt <momiimt@gmail.com>
2022-06-17 18:24:12 +0900

    👍 add: implement kdb, kdb_modoki, twins_notify

--- Tukuba_Automation.nimble
+++ Tukuba_Automation.nimble
@@ -1,13 +1,15 @@
 # Package
 
 version       = "0.1.0"
-author        = "momeemt"
-description   = "A new awesome nimble package"
+author        = "kikumyon"
+description   = ""
 license       = "MIT"
 srcDir        = "src"
 bin           = @["Tukuba_Automation"]
 
-
 # Dependencies
 
 requires "nim >= 1.6.6"
+requires "cligen == 1.5.24"
+requires "halonium == 0.2.6"
+requires "fusion == 1.1"
--- src/Tukuba_Automation.nim
+++ src/Tukuba_Automation.nim
@@ -1,5 +1,73 @@
-# This is just an example to get you started. A typical binary package
-# uses this file as the main entry point of the application.
+{.experimental: "caseStmtMacros".}
+
+import std/[options, strformat]
+import fusion/matching
+import halonium
+
+type
+  ITFWebServices {.pure.} = enum
+    kdb = "KdB"
+    kdb_modoki = "KdBもどき"
+    twins = "Twins"
+
+proc kdb_boilerplate (search_query, url, textfield_selector, submit_button_selector, error_sentence: string) =
+  let session = createSession(Chrome)
+  session.navigate(url)
+
+  case session.waitForElement(textfield_selector)
+  of Some(@textfield_element):
+    textfield_element.sendKeys(search_query)
+  of None():
+    stderr.writeLine error_sentence
+    session.stop()
+    quit(1)
+  
+  case session.waitForElement(submit_button_selector)
+  of Some(@submit_button_element):
+    submit_button_element.click()
+  of None():
+    stderr.writeLine error_sentence
+    session.stop()
+    quit(1)
+
+func get_error_sentence (site_kind: ITFWebServices): string =
+  result = &"何らかの問題が発生して{site_kind}のコンテンツを取得することに失敗しました"
+
+proc kdb (search_query: string): int =
+  const
+    url = "https://kdb.tsukuba.ac.jp"
+    textfield_selector = "#freeWord"
+    submit_button_selector = """input[type="button"][value="検索"]"""
+    error_sentence = get_error_sentence(ITFWebServices.kdb)
+  kdb_boilerplate(search_query, url, textfield_selector, submit_button_selector, error_sentence)
+
+proc kdb_modoki (search_query: string): int =
+  const
+    url = "https://make-it-tsukuba.github.io/alternative-tsukuba-kdb/"
+    textfield_selector = "#keyword"
+    submit_button_selector = "#submit"
+    error_sentence = get_error_sentence(ITFWebServices.kdb_modoki)
+  kdb_boilerplate(search_query, url, textfield_selector, submit_button_selector, error_sentence)
+
+proc twins_notify (limit: int = 100000000): int =
+  let session = createSession(Chrome, browserOptions=chromeOptions(args=["--headless"]))
+  session.navigate("https://twins.tsukuba.ac.jp/")
+  
+  let tr_arrays = session.waitForElements("""//*[@id="keiji-portlet"]/table/tbody/tr""", XPathSelector)
+  for index in 1..max(0, min(limit, tr_arrays.len)):
+    case session.waitForElement(&"""//*[@id="keiji-portlet"]/table/tbody/tr[{index}]/td/a""", XPathSelector)
+    of Some(@anchor_text):
+      echo anchor_text.text
+    of None():
+      stderr.writeLine get_error_sentence(ITFWebServices.twins)
+      session.stop()
+      quit(1)
+  session.stop()
 
 when isMainModule:
-  echo("Hello, World!")
+  import cligen
+  dispatchMulti(
+    [kdb],
+    [kdb_modoki],
+    [twins_notify]
+  )
